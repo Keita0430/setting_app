edfbcd49bf80f851541bbbc0ca68e0211cad05f8
Ubuntu <ubuntu@ip-172-31-17-63.us-east-2.compute.internal>
2021-04-12 17:02:42 +0000

    Finish associateion

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
+  has_many :user_rooms
+  has_many :messages
+  
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
