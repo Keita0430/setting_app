8d2683a959df3680110a7fe30043606c7d148753
Ubuntu <ubuntu@ip-172-31-17-63.us-east-2.compute.internal>
2021-04-21 15:00:12 +0000

    Implement advanced chat-function

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -24,4 +24,43 @@ document.addEventListener('turbolinks:load', () => {
             messageContainer.insertAdjacentHTML('beforeend', data['message'])
         }
     })
+    
+    
+    
+    
+    // -------------- 最新メッセージを表示 ------------------
+    const documentElement = document.documentElement
+    // js.erb 内でも使用できるように変数を決定
+    window.messageContent = document.getElementById('message_content')
+    // 一番下まで移動する関数。js.erb 内でも使用できるように変数を決定
+    window.scrollToBottom = () => {
+        window.scroll(0, documentElement.scrollHeight)
+    }
+    
+    /*global scrollToBottom*/
+    // 最初にページ一番下へ移動させる
+    scrollToBottom()
+    
+    
+    
+    
+    // -------------- 送信ボタンの有効化・無効化 ----------
+    /*global messageContent*/ /*global messageButton*/
+    const button_activation = () => {
+        if (messageContent.value === '') {
+            messageButton.classList.add('disabled')
+        } else {
+            messageButton.classList.remove('disabled')
+        }
+    }
+    
+    // フォームに入力した際の動作
+    messageContent.addEventListener('input', () => {
+        button_activation()
+    })
+    
+    // 送信ボタンが押された時にボタンを無効化
+    messageButton.addEventListener('click', () => {
+        messageButton.classList.add('disabled')
+    })
 })
