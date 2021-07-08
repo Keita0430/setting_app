5836d418bc5ce4ac8b4786dc0ccd8a4d71980c45
Ubuntu <ubuntu@ip-172-31-30-117.us-east-2.compute.internal>
2021-07-08 07:10:22 +0000

    Modify routing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,13 +2,4 @@ class StaticPagesController < ApplicationController
   def home
     @articles = Article.all
   end
-
-  def help
-  end
-  
-  def about
-  end
-  
-  def review
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
   get 'articles/show'
   mount ActionCable.server => "/cable"
   root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/review', to: 'static_pages#review'
   get  '/signup', to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
