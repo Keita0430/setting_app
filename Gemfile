ed44f2f4fd3abafe9714fa486d0fd8521697b4d4
Ubuntu <ubuntu@ip-172-31-20-119.us-east-2.compute.internal>
2021-08-28 14:52:42 +0000

    Add gem production

--- Gemfile
+++ Gemfile
@@ -40,7 +40,8 @@ group :test do
 end
 
 group :production do
-  gem 'mysql2'
+  gem 'pg', '1.1.4'
+  # gem 'mysql2'
   gem 'unicorn'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_180010) do
+ActiveRecord::Schema.define(version: 2021_07_11_180014) do
 
   create_table "articles", force: :cascade do |t|
     t.text "content"
