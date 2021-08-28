d883efab26b48c945e5e7361dd9cceef0ccac20a
Ubuntu <ubuntu@ip-172-31-20-119.us-east-2.compute.internal>
2021-08-28 15:54:30 +0000

    Destroy migration file

--- db/migrate/20210512091244_change_datatype_gender_of_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypeGenderOfUsers < ActiveRecord::Migration[6.0]
-  def change
-    change_column :users, :gender, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_180014) do
+ActiveRecord::Schema.define(version: 2021_08_28_154639) do
 
   create_table "articles", force: :cascade do |t|
     t.text "content"
@@ -73,7 +73,6 @@ ActiveRecord::Schema.define(version: 2021_07_11_180014) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
-    t.integer "gender"
     t.string "avatar"
     t.string "nickname"
     t.string "age"
